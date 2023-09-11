edd22b9cd1065ce63b411078103784065eef41e4
= <krishnasurisetty89@gmail.com>
2023-09-11 21:25:58 +0530

    Initialized Basic Spring Boot Application

---
+++ src/main/java/com/learningmy/springsecurityconcept/SpringSecurityConceptApplication.java
@@ -0,0 +1,13 @@
+package com.learningmy.springsecurityconcept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityConceptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learningmy/springsecurityconcept/SpringSecurityConceptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningmy.springsecurityconcept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityConceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
